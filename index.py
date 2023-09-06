eb5494afb1569e7f5a2dc09fa584624a5da91e8a
may@ap-com.co.jp <may@ap-com.co.jp>
2023-09-06 02:36:13 +0000

    remove attributes

--- index.py
+++ index.py
@@ -1,10 +1,7 @@
 import streamlit as st
 
 text_input = st.text_input(
-        "Enter some text 👇",
-        disabled=st.session_state.disabled,
-        placeholder=st.session_state.placeholder,
-    )
+        "Enter some text 👇")
 
 if text_input:
     st.write("You entered: ", text_input)
