f1ae9f2095b3a6dfb2baf9d461861efb3e087b64
may@ap-com.co.jp <may@ap-com.co.jp>
2023-09-08 05:00:10 +0000

    add qa bot

--- index.py
+++ index.py
@@ -1,7 +1,47 @@
-import streamlit as st
+import streamlit as st 
+import numpy as np 
+import json
+import requests
+#text_input = st.text_input(
+#        "Enter some text 👇")
 
-text_input = st.text_input(
-        "Enter some text 👇")
+#if text_input:
+#    st.write("You entered: ", text_input)
 
-if text_input:
-    st.write("You entered: ", text_input)
+
+
+st.title('Databricks Q&A bot')
+#st.header('Databricks Q&A bot')
+
+def generate_answer(question):
+  # Driver Proxyと異なるクラスター、ローカルからDriver Proxyにアクセスする際にはパーソナルアクセストークンを設定してください
+  token = "" 
+  url = "http://127.0.0.1:7777/"
+
+  headers = {
+      "Content-Type": "application/json",
+      "Authentication": f"Bearer {token}"
+  }
+  data = {
+    "prompt": question
+  }
+
+  response = requests.post(url, headers=headers, data=json.dumps(data))
+  if response.status_code != 200:
+    raise Exception(
+       f"Request failed with status {response.status_code}, {response.text}"
+    )
+  
+  response_json = response.json()
+  return response_json
+
+question = st.text_input("**質問**")
+
+if question != "":
+  response = generate_answer(question)
+
+  answer = response["answer"]
+  source = response["source"]
+
+  st.write(f"**回答:** {answer}")
+  st.write(f"**ソース:** [{source}]({source})")
