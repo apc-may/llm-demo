dff06af537dfc8b44e0652c3b2a1aebdb948a905
may@ap-com.co.jp <may@ap-com.co.jp>
2023-09-12 09:27:08 +0000

    add test graph

--- index.py
+++ index.py
@@ -51,9 +51,19 @@ if question != "":
 
     st.write(f"**回答:** {answer}")
 
-    fig = ff.create_distplot(
-        answer, answer.keys(), bin_size=[.1, .25, .5])
+    # Add histogram data
+x1 = np.random.randn(200) - 2
+x2 = np.random.randn(200)
+x3 = np.random.randn(200) + 2
 
-    # Plot!
-    st.plotly_chart(fig, use_container_width=True)
-    #response.raise_for_status()
+# Group data together
+hist_data = [x1, x2, x3]
+
+group_labels = ['Group 1', 'Group 2', 'Group 3']
+
+# Create distplot with custom bin_size
+fig = ff.create_distplot(
+        hist_data, group_labels, bin_size=[.1, .25, .5])
+
+# Plot!
+st.plotly_chart(fig, use_container_width=True)
