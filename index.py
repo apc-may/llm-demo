44a2027c5008c5024305404551fb61b835de4652
may@ap-com.co.jp <may@ap-com.co.jp>
2023-09-13 04:13:24 +0000

    test max iteration

--- index.py
+++ index.py
@@ -167,7 +167,7 @@ ask_button = ""
 language = st.selectbox('language',['English','日本語'])
 
 if df.shape[0] > 0:
-    agent = create_pandas_dataframe_agent(OpenAI(temperature=0, max_tokens=1000), df, memory=state['memory'], verbose=True, return_intermediate_steps=True)
+    agent = create_pandas_dataframe_agent(OpenAI(temperature=0, max_tokens=1000), df, memory=state['memory'], verbose=True, return_intermediate_steps=True, max_iterations=10)
     user_input = get_text()
     ask_button = st.button('ask')
 else:
